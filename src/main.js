6831857b2ada2bdae1b2d33ae80f9a634dbeeac6
Jeado Ko <haibane84@gmail.com>
2013-10-15 21:27:02 +0900

    테스트용 index 추가

---
+++ src/main.js
@@ -0,0 +1,25 @@
+/**
+* ngBookmark Module
+*
+* Description
+*/
+angular.module('ngBookmark', []).
+	controller('listCtrl', ['$scope', function ($scope) {
+		var bookmarkList = [
+			{ title : "google", url:"google.com" },
+			{ title : "google1", url:"google.com" },
+			{ title : "google2", url:"google.com" },
+			{ title : "google3", url:"google.com" },
+			{ title : "google4", url:"google.com" },
+			{ title : "google5", url:"google.com" },
+			{ title : "google6", url:"google.com" },
+			{ title : "google7", url:"google.com" },
+			{ title : "google8", url:"google.com" }
+		];
+
+		$scope.bookmarkList = bookmarkList;
+		$scope.showDetail = function showDetail (bookmark) {
+			console.log(bookmark);
+			// body...
+		};
+	}]);
